f17a862dcadef947bf38c2a803d586654476e7bb
Pomin Wu <pomin5@gmail.com>
2015-11-07 09:42:51 +0800

    add generate-ct

--- src/markovmusic/chain.clj
+++ src/markovmusic/chain.clj
@@ -8,26 +8,37 @@
     ))
 
 (defn generate
-  "Generate an infinite sequence with frequency matrix."
+  "Generate an infinite sequence with discrete Markov chain"
   [frequency-matrix current]
   (let [next-value (generate-next frequency-matrix current)]
     (lazy-seq
       (cons next-value
           (generate frequency-matrix next-value)))))
 
-(def duration-vector [100 200 200 200])
+(defn exponetial-rand
+  "Generate a random value between 0 and infinite by exponential distrubtion of given rate."
+  [rate]
+  (/ (Math/log (- 1 (rand))) (- rate)))
 
-(defn generate-next-duration
-  [frequency-matrix current]
-  {:value (generate-next frequency-matrix (current :value)) :duration (duration-vector (int (rand (count duration-vector))))})
+(defn generate-next-ct
+  [frequency-matrix holding-rates current]
+  (let [next-value    (generate-next frequency-matrix (current :value))
+        next-duration (exponetial-rand (holding-rates next-value))]
+    {:value next-value :duration next-duration}))
 
-(defn generate-duration
-  "Generate an infinite sequence of values with duration."
-  [frequency-matrix current]
-  (let [next-value (generate-next-duration frequency-matrix current)]
-    (lazy-seq
-      (cons current
-            (generate-duration frequency-matrix next-value)))))
+(defn generate-ct
+  "Generate an infinite sequence of values and duration with CTMC."
+  ([frequency-matrix holding-rates current]
+   (let [next-value (generate-next-ct frequency-matrix holding-rates current)]
+     (lazy-seq
+       (cons current
+             (generate-ct frequency-matrix holding-rates next-value)))))
+
+  ([frequency-matrix current]
+   (generate-ct frequency-matrix
+                (zipmap (keys frequency-matrix) (map (fn [x] 0.005) (range (count frequency-matrix))))
+                current))
+  )
 
 ;(def reich-degrees [:vi :vii :i+ :_ :vii :_ :i+ :vii :vi :_ :vii :_])
 ;(generate-next reich-frequency-matrix :vi)
--- src/markovmusic/core.clj
+++ src/markovmusic/core.clj
@@ -15,16 +15,14 @@
 (def happy-pitches (degrees->pitches happy-degrees :diatonic :C4))
 (player/play-fixed-length-notes piano (now) happy-pitches 200)
 
-(stop)
-
-;(player/play-fixed-length-notes piano (now) reich-pitches 200)
 (let [sequence (take 32 (chain/generate reich-frequency-matrix nil))]
   (player/play-fixed-length-notes piano (now) (degrees->pitches sequence :diatonic :C4) 200)
-  ;(player/play-fixed-length-notes piano (+ (now) 1600) (degrees->pitches sequence :diatonic :G3) 200)
   )
 
-(let [sequence (take 32 (chain/generate-duration reich-frequency-matrix {:value nil :duration 0}))]
-  (player/play piano (now) sequence :minor :A3)
+(let [sequence (cycle (take 32 (chain/generate-ct reich-frequency-matrix {:value nil :duration 0})))]
+  (player/play piano (now) sequence :major :A3)
   )
 
 (midi/read-file "resources/WTCBkI/Fugue1.mid" 1)
+
+(stop)
