9ea56fb7e984797358b36e93d7106b0058ad1e5e
Pomin Wu <pomin5@gmail.com>
2015-11-07 13:06:30 +0800

    add two tigers

--- src/markovmusic/core.clj
+++ src/markovmusic/core.clj
@@ -7,6 +7,8 @@
 				[overtone.inst.piano]
         [overtone.inst.drum]))
 
+; Reich degrees
+
 (def reich-degrees [:vi :vii :i+ :_ :vii :_ :i+ :vii :vi :_ :vii :_])
 (def reich-pitches (degrees->pitches reich-degrees :diatonic :C4))
 (def reich-frequency-matrix (chain/generate-frequency-matrix reich-degrees))
@@ -18,6 +20,22 @@
 
 (let [sequence (cycle (take 32 (chain/generate-ct reich-frequency-matrix {:value nil :duration 0})))]
   (player/play piano (now) sequence :major :A3)
+  )
+
+; Two tigers
+
+(def two-tigers [:i nil :ii nil :iii nil :i nil
+                 :i nil :ii nil :iii nil :i nil
+                 :iii nil :iv nil :v nil nil nil
+                 :iii nil :iv nil :v nil nil nil
+                 :v :vi :v :iv :iii nil :i nil
+                 :v :vi :v :iv :iii nil :i nil
+                 :i nil :v- nil :i nil nil nil
+                 :i nil :v- nil :i nil nil nil])
+
+
+(let [sequence (take 64 (chain/generate (chain/generate-frequency-matrix two-tigers) nil))]
+  (player/play-fixed-length-notes piano (now) (degrees->pitches sequence :major :C4) 200)
   )
 
 (player/play-duration-notes piano (now) (midi/read-file "resources/WTCBkI/Fugue1.mid" 1))
